5f6141c48fdbcaf82f41d10cac674fb7557d4022
Nate <nate.m.zimmerman@gmail.com>
2020-05-25 21:27:00 -0500

    clean updates

--- demo.lua
+++ demo.lua
@@ -13,8 +13,11 @@ local data_dis = Dissector.get("data") -- The traditional data dissector
 my_proto.fields = {}
 local f = my_proto.fields
 f.header = ProtoField.uint8("my_proto.header","Data Header",base.HEX)
+-- Define request field as upper bit of a byte 
 f.req = ProtoField.uint8("my_proto.req","Req/Res",base.HEX,{[0]='Request',[1]='Response'},0x80)
+-- Define address field as lower bits of a byte 
 f.adr = ProtoField.uint8("my_proto.req","Address",base.HEX,nil,0x7F)
+-- Define other fields 
 f.value = ProtoField.uint16("my_proto.value","Value",base.DEC)
 f.seq = ProtoField.uint8("my_proto.seq","Sequence",base.DEC)
 f.len = ProtoField.uint8("my_proto.len","Length",base.DEC)
@@ -29,11 +32,11 @@ function my_proto.dissector(buf,pinfo,tree)
 	local offset = 0 
 	local header_sub = subtree:add(f.header,buf(offset,1)) -- Add header byte 
 	header_sub:add(f.req,buf(offset,1)) -- dissect req. bit 
-	header_sub:add(f.adr,buf(offset,1))
+	header_sub:add(f.adr,buf(offset,1)) -- bits relating to address 
 	info_str = info_str .. 'ADR:' .. bit.band(buf(0,1):uint(), 0x7F) .. ', '
 	offset = offset + 1
 	
-	subtree:add(f.seq,buf(offset,1))
+	subtree:add(f.seq,buf(offset,1)) -- Add sequence field 
 	info_str = info_str .. 'Seq[' .. buf(1,1):uint() .. '], '
 	offset = offset + 1
 	
@@ -48,11 +51,12 @@ function my_proto.dissector(buf,pinfo,tree)
 		subtree:add(f.len,buf(offset,1))
 		offset = offset + 1
 		subtree:add(f.data,buf(offset,buf:len() - offset))
+		-- Optional, call data dissector w/ additional data 
 		data_dis:call(buf:range(offset):tvb(),pinfo,tree)
 	end 
 	
-	pinfo.cols.protocol = 'My Proto'
-	pinfo.cols.info = info_str
+	pinfo.cols.protocol = 'My Proto' -- Updates protocol column 
+	pinfo.cols.info = info_str -- Updates info column 
 end
 
 -- Get wiretap dissector table 
